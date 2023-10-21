76bd37b1ca270b084297b4d1f4ab0c65269f70b9
Anshdwivedi12 <123242841+Anshdwivedi12@users.noreply.github.com>
2023-10-21 08:16:41 +0530

    Add files via upload

---
+++ real-estate website {backend} (1)/project/components/footer.php
@@ -0,0 +1,36 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i><span>8418036247</span></a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i><span>8112721689</span></a>
+         <a href="rishujii396@gmail.com"><i class="fas fa-envelope"></i><span>rishabhdwivedi396@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>kanpur, india - 208019</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#https://x.com/Rishu15530599?t=4KKP1HHoecnW1Gk1-NR88w&s=09"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#https://www.linkedin.com/in/rishabh-dwivedi-8149651a3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Rishabh Dwivedi</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
